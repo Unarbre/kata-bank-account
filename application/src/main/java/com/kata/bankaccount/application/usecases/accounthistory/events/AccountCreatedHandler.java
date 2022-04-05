a79d17085eed20dc3282f03b3e0d66295a27c4a7
Simon <halimisimon@gmail.com>
2022-04-05 19:15:38 +0200

    [REFACTO] History handlers: Rename to fit actions handled

--- application/src/main/java/com/kata/bankaccount/application/usecases/accounthistory/events/AccountCreatedHandler.java
+++ application/src/main/java/com/kata/bankaccount/application/usecases/accounthistory/events/AccountCreatedHandler.java
@@ -1,10 +1,10 @@
 package com.kata.bankaccount.application.usecases.accounthistory.events;
 
-import com.kata.bankaccount.common.events.WithdrawProceeded;
+import com.kata.bankaccount.common.events.AccountCreated;
 import com.kata.bankaccount.domain.history.AccountId;
+import com.kata.bankaccount.domain.history.IHistories;
 import com.kata.bankaccount.domain.history.History;
 import com.kata.bankaccount.domain.history.HistoryId;
-import com.kata.bankaccount.domain.history.IHistories;
 import io.jkratz.mediator.core.EventHandler;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
@@ -13,27 +13,25 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 
 import static com.kata.bankaccount.domain.history.HistoryType.CREATION;
-import static com.kata.bankaccount.domain.history.HistoryType.WITHDRAW;
+
 
 @Component
 @AllArgsConstructor
-public class AddWithdrawProceedHistory implements EventHandler<WithdrawProceeded> {
+public class AccountCreatedHandler implements EventHandler<AccountCreated> {
 
     private final IHistories histories;
 
     @Override
-    public void handle(@NonNull WithdrawProceeded withdrawProceeded) {
+    public void handle(@NonNull AccountCreated accountCreated) {
         var history = History.create()
-                .accountId(new AccountId(withdrawProceeded.id()))
+                .accountId(new AccountId(accountCreated.id()))
                 .id(new HistoryId(histories.getNextId()))
                 .date(LocalDate.now())
-                .newBalance(withdrawProceeded.newBalance())
-                .previousBalance(withdrawProceeded.newBalance().add(withdrawProceeded.amount()))
-                .type(WITHDRAW)
+                .newBalance(accountCreated.initialBalance())
+                .type(CREATION)
                 .build();
 
 
         this.histories.save(history.to());
-
     }
 }
--- application/src/main/java/com/kata/bankaccount/application/usecases/accounthistory/events/WithdrawProceededHandler.java
+++ application/src/main/java/com/kata/bankaccount/application/usecases/accounthistory/events/WithdrawProceededHandler.java
@@ -1,10 +1,10 @@
 package com.kata.bankaccount.application.usecases.accounthistory.events;
 
-import com.kata.bankaccount.common.events.AccountCreated;
+import com.kata.bankaccount.common.events.WithdrawProceeded;
 import com.kata.bankaccount.domain.history.AccountId;
-import com.kata.bankaccount.domain.history.IHistories;
 import com.kata.bankaccount.domain.history.History;
 import com.kata.bankaccount.domain.history.HistoryId;
+import com.kata.bankaccount.domain.history.IHistories;
 import io.jkratz.mediator.core.EventHandler;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
@@ -13,25 +13,27 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 
 import static com.kata.bankaccount.domain.history.HistoryType.CREATION;
-
+import static com.kata.bankaccount.domain.history.HistoryType.WITHDRAW;
 
 @Component
 @AllArgsConstructor
-public class AddAccountCreatedHistory implements EventHandler<AccountCreated> {
+public class WithdrawProceededHandler implements EventHandler<WithdrawProceeded> {
 
     private final IHistories histories;
 
     @Override
-    public void handle(@NonNull AccountCreated accountCreated) {
+    public void handle(@NonNull WithdrawProceeded withdrawProceeded) {
         var history = History.create()
-                .accountId(new AccountId(accountCreated.id()))
+                .accountId(new AccountId(withdrawProceeded.id()))
                 .id(new HistoryId(histories.getNextId()))
                 .date(LocalDate.now())
-                .newBalance(accountCreated.initialBalance())
-                .type(CREATION)
+                .newBalance(withdrawProceeded.newBalance())
+                .previousBalance(withdrawProceeded.newBalance().add(withdrawProceeded.amount()))
+                .type(WITHDRAW)
                 .build();
 
 
         this.histories.save(history.to());
+
     }
 }
